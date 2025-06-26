ca83f293ccd2c14f2ba7ca4425e7b2361a557e6e
Matt Bennett <144359294+filmteacher@users.noreply.github.com>
2025-06-26 19:09:11 -0400

    FInal Project

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,35 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int month = 0; // num month = 0
+        String trash = ""; // use for bad input which will read as a String
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter your birth month as an integer between 1 and 12: "); // output "Enter your birth month as an integer between 1 and 12: "
+
+        if(in.hasNextInt()) // OK safe to read in an integer
+        {
+            month = in.nextInt();  // input month
+            in.nextLine(); // clears the newline from the buffer
+
+            if (month >=1 && month <=12) // if month >= 1 AND month <= 12 then
+            {
+                System.out.println("Your birth month is: " + month); // output "Your birth month is “ + month
+            }
+            else // else
+            {
+                System.out.println("You entered an incorrect month value: " + month); // output "You entered an incorrect month value: “ + month
+            } // end if
+        }
+        else // Not an int can't use nextInt() read as String with nextLine() instead
+        {
+            trash = in.nextLine(); // Ok have to read the input as a String
+            System.out.println("You said your birth month was: " + trash);
+            System.out.println("Run the program again and enter a valid birth month from 1-12.");
         }
     }
 }
